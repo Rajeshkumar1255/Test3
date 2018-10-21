6a97d8173df298a1ffc85db00ae8c087c2378c67
Sony <rajeshku1255@gmail.com>
2018-10-21 16:20:57 +0530

    Add 1

--- Test3/src/com/pack3/Demo.java
+++ Test3/src/com/pack3/Demo.java
@@ -1,5 +1,13 @@
 package com.pack3;
 
-public class Demo {
-
+public class Demo
+{
+ public static void main(String[] args) 
+ {
+	 int i=10 ,j=30;
+	 int k=i+j;
+	 
+	 System.out.println(" add " + k );
+	 
+}
 }
